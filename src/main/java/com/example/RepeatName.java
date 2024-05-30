73d1d2b02e831224eb42bbed673e1403b198c4b9
hemant1317 <hemant1317@gmail.com>
2024-05-30 08:45:12 +0530

    committing for Assignment 4

---
+++ src/main/java/com/example/RepeatName.java
@@ -0,0 +1,14 @@
+package com.example;
+
+    import java.util.Scanner;
+
+        public static void main(String[] args); {
+
+Scanner scanner = scanner(System.in);
+
+//repeat the name 10 times
+for (int i = 0; i < 10; i++) {
+    System.out.println("For Loop Count" + i);
+}
+
+//
